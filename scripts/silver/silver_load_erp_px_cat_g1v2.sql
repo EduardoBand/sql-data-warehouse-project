9e3d037a3c51bdfef4ccd7e3fafcf33cd1b28ad5
Eduardo Lupinetti Bandeira <138628450+EduardoBand@users.noreply.github.com>
2026-02-06 13:47:34 -0300

    Modify INSERT statement in silver_load_erp_px_cat_g1v2.sql

--- scripts/silver/silver_load_erp_px_cat_g1v2.sql
+++ scripts/silver/silver_load_erp_px_cat_g1v2.sql
@@ -31,8 +31,10 @@ Target Table:
 
 PRINT'>> Truncating Table: silver.erp_px_cat_g1v2';
 TRUNCATE TABLE silver.erp_px_cat_g1v2
+    
 PRINT '>> Inserting Data into silver.erp_px_cat_g1v2';
-INSERT INTO silver.erp_px_cat_g1v2 (id,
+INSERT INTO silver.erp_px_cat_g1v2 (
+id,
 cat,
 subcat,
 maintenance)
